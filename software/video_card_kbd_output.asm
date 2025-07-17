8e845ad3e55c500cdd2bad6830fb4ab5de8a94d5
Artem Kalinchuk <artem9@gmail.com>
2025-07-17 11:09:51 -0400

    Binary

--- software/video_card_kbd_output.asm
+++ software/video_card_kbd_output.asm
@@ -1,109 +1,147 @@
-; Author:    Artem Kalinchuk
-; Created:   01.30.2025
-;
-; Prints KBD input on screen using Len Bayles
-; video card. This supports the return key
-; and also the rubout key for deleting.
-;
-; DATA PORT: 5
-; COLUMN PORT: 6
-; ROW PORT: 7
+/***************************************************
+* Author:    Artem Kalinchuk
+* Created:   02.02.2025
+*
+* Clears the screen of the video card
+* (designed by Len Bayles) and receives
+* input from a keyboard that is then printed
+* on the screen.
+*
+* DATA PORT: 5 (14)
+* COLUMN PORT: 6 (15)
+* ROW PORT: 7 (16)
+***************************************************/
 
-INIT:
-  LCI 0x00              ; row pos; 026 000
-  LDI 0x00              ; col pos; 036 000
-  LAI 0x00              ; 006 000
-  OUT 6                 ; 135
-  OUT 7                 ; 137
+.target "8008"
+* = $2000
+
+INITIALIZE:
+  LAI 0x00              
+  OUT 14                
+  OUT 15                
+  CAL CLEAR             
+  LCI 0x00              
+  LDI 0x00              
+  LAI 0x00              
+  OUT 14                 
+  OUT 15
+  JMP LISTEN_KBD
+
+CLEAR:
+  CAL INITCLEAR         
+  /* clear last two positions */
+  LAI 0x20              
+  OUT 13                
+  OUT 13                
+  RET                   
+
+INITCLEAR:
+  LBI 0xFF              
+  LEI 0x01              
+CLEARLOOP:
+  LAI 0x20              
+  OUT 13                
+  DCB                   
+  LAB                   
+  CPI 0x00              
+  JTZ DECREMENT         
+  JMP CLEARLOOP         
+DECREMENT:
+  LAE                   
+  CPI 0x00              
+  RTZ                   
+  LBI 0xFF              
+  DCE                   
+  JMP CLEARLOOP
 
 LISTEN_KBD:
-  INP 0                 ; 101
-  RAL                   ; 022
-  JFC LISTEN_KBD        ; 100 010 000
+  INP 0                
+  RAL                  
+  JFC LISTEN_KBD       
 
-  RAR                   ; 032
-  NDI 0x7F              ; 044 177
-  CAL PROCESS_CHAR      ; 106 033 000
+  RAR                  
+  NDI 0x7F             
+  CAL PROCESS_CHAR     
 
 LISTEN_KBD_LOW:
-  INP 0                 ; 101
-  RAL                   ; 022
-  JTC LISTEN_KBD_LOW    ; 140 023 000
-  JMP LISTEN_KBD        ; 104 010 000
+  INP 0                
+  RAL                  
+  JTC LISTEN_KBD_LOW   
+  JMP LISTEN_KBD       
 
 PROCESS_CHAR:
-  CPI 0x0D              ; CR ; 074 015
-  JTZ INC_ROW           ; 150 140 000
-  CPI 0x7F              ; DELETE ; 074 177
-  JTZ DELETE            ; 150 051 000
-  CAL PRINT             ; 106 121 000
-  RET                   ; 007
+  CPI 0x0D             
+  JTZ INC_ROW          
+  CPI 0x7F             
+  JTZ DELETE           
+  CAL PRINT            
+  RET                  
 
 DELETE:
-  CAL DEC_POS           ; 106 064 000
-  NOP                   ; for future deletion ; 311
-  NOP                   ; 311
-  NOP                   ; 311
-  NOP                   ; 311
-  NOP                   ; 311
-  NOP                   ; 311
-  NOP                   ; 311
-  RET                   ; 007
+  CAL DEC_POS          
+  NOP                  
+  NOP                  
+  NOP                  
+  NOP                  
+  NOP                  
+  NOP                  
+  NOP                  
+  RET                  
 
 DEC_POS:
-  LAD                   ; 303
-  CPI 0x00              ; 074 000
-  JTZ DEC_ROW           ; 150 076 000
-  DCD                   ; 031
-  LAD                   ; 303
-  OUT 6                 ; 135
-  RET                   ; 007
+  LAD                  
+  CPI 0x00             
+  JTZ DEC_ROW          
+  DCD                  
+  LAD                  
+  OUT 14               
+  RET                  
 
 DEC_ROW:
-  LDI 0x20              ; 036 040
-  LAD                   ; 303
-  OUT 6                 ; 135
-  LAC                   ; 302
-  CPI 0x00              ; 074 000
-  JTZ JUMP_TO_BOTTOM    ; 150 114 000
-  DCC                   ; 021
-  LAC                   ; 302
-  OUT 7                 ; 137
-  RET                   ; 007
+  LDI 0x20             
+  LAD                  
+  OUT 14               
+  LAC                  
+  CPI 0x00             
+  JTZ JUMP_TO_BOTTOM   
+  DCC                  
+  LAC                  
+  OUT 15               
+  RET                  
 
 JUMP_TO_BOTTOM:
-  LCI 0x10              ; 026 020
-  LAC                   ; 302
-  OUT 7                 ; 137
-  RET                   ; 007
+  LCI 0x10             
+  LAC                  
+  OUT 15               
+  RET                  
 
 PRINT:
-  OUT 5                 ; 133
-  CAL INC_POS           ; 106 126 000
-  RET                   ; 007
+  OUT 13               
+  CAL INC_POS          
+  RET                  
 
 INC_POS:
-  LAD                   ; 303
-  CPI 0x20              ; 074 040
-  JTZ INC_ROW           ; 150 140 000
-  IND                   ; 030
-  LAD                   ; 303
-  OUT 6                 ; 135
-  RET                   ; 007
+  LAD                  
+  CPI 0x20             
+  JTZ INC_ROW          
+  IND                  
+  LAD                  
+  OUT 14               
+  RET                  
 
 INC_ROW:      
-  LDI 0x00              ; 036 000
-  LAD                   ; 303
-  OUT 6                 ; 135
-  INC                   ; 020
-  LAC                   ; 302
-  CPI 0x10              ; 074 020
-  JTZ RESET_ROW         ; 150 155 000
-  OUT 7                 ; 137
-  RET                   ; 007
+  LDI 0x00             
+  LAD                  
+  OUT 14               
+  INC                  
+  LAC                  
+  CPI 0x10             
+  JTZ RESET_ROW        
+  OUT 15               
+  RET                  
 
 RESET_ROW:      
-  LCI 0x00              ; 026 000
-  LAC                   ; 302
-  OUT 7                 ; 137
-  RET                   ; 007
+  LCI 0x00             
+  LAC                  
+  OUT 15               
+  RET                  
